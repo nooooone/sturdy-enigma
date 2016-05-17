ac073112d6bf06585514504b862808ec795fb66f
nooooone <hj9lt@slipry.net>
2016-05-17 15:40:16 -0700

    Cleanup
    Mostly removing vestigial stuff, adding documentation, moving things
    around.

--- indexer/__init__.py
+++ indexer/__init__.py
@@ -6,18 +6,26 @@ import threading
 from threading import Thread, Lock
 from typing import List, Tuple, ByteString, Dict
 
+# Type aliases
+IndexStore = Dict
+Result = ByteString
+State = namedtuple('State', ['index_store', 'lock'])
+
+# Constants
 COMMANDS = ['INDEX', 'REMOVE', 'QUERY']
 PKGNAME_RE = re.compile(r'^[a-z0-9-_+A-Z]+$')
 DEPLIST_RE = re.compile(r'^([a-z0-9-_+A-Z],?)+$')
-IndexStore = Dict
-State = namedtuple('State', ['index_store', 'lock'])
+ERROR = b'ERROR\n'
+FAIL = b'FAIL\n'
+OK = b'OK\n'
 
 class CommandParsingError(Exception): pass
 
 class Package:
-    """Represents a package. The only data enumerated in the challenge
-    description are package name and its dependencies, so that's all that is
-    tracked here."""
+    """Represents a package. Maintains a list of both children (packages that
+    depend on this one) and dependencies (packages that this one depends on) as
+    well as the package's name.
+    """
     __slots__ = ['dependencies', 'name', 'children']
 
     def __init__(self,
@@ -27,51 +35,18 @@ class Package:
         self.dependencies = dependencies
         self.children = []
 
-    def __hash__(self) -> int:
-        return hash(self.name)
-
     def __repr__(self) -> str:
-        return "{}|{}".format(self.name, ','.join(map(str, self.dependencies)))
+        return "<Package {}, depending on {}>".format(
+            self.name, ','.join(map(str, self.dependencies))
+        )
 
     def __str__(self) -> str:
         return self.name
 
-class Result:
-    """Contains a response code as self.code; one of 'fail', 'ok', or 'error'.
-    Uppercases the code before converting to a string so it can be passed back
-    out over the network"""
-    __slots__ = ['code', '_bs']
-
-    def __init__(self, code: str) -> None:
-        self._bs = None
-        if code not in ['fail', 'ok', 'error']:
-            raise Exception("Bad argument to Result, got: '{}'".format(code))
-
-        self.code = code
-
-    def __repr__(self) -> str:
-        return self.code
-
-    def __str__(self) -> str:
-        return self.code.upper() + '\n'
-
-    @property
-    def bytes(self) -> ByteString:
-        if self._bs is None:
-            self._bs = str(self).encode('utf-8')
-        return self._bs
-
-ERROR = Result('error')
-FAIL = Result('fail')
-OK = Result('ok')
-
-def index(state: State,
-          pkg_name: str,
-          deps: List[str]) -> Result:
-    """For `INDEX` commands, the server returns `OK\n` if the package could be
-    indexed or if it was already present. It returns `FAIL\n` if the package
-    cannot be indexed because some of its dependencies aren't indexed yet and
-    need to be installed first."""
+def index(state: State, pkg_name: str, deps: List[str]) -> Result:
+    """Returns OK if the package was found or successfully indexed; FAIL
+    otherwise.
+    """
     with state.lock:
         store = state.index_store
         pkg = store.get(pkg_name)
@@ -88,12 +63,10 @@ def index(state: State,
                 dep_pkg.children += [pkg_name]
             return OK
 
-def remove(state: State,
-           pkg_name: str) -> Result:
-    """For `REMOVE` commands, the server returns `OK\n` if the package could be
-    removed from the index. It returns `FAIL\n` if the package could not be
-    removed from the index because some other indexed package depends on it. It
-    returns `OK\n` if the package wasn't indexed."""
+def remove(state: State, pkg_name: str) -> Result:
+    """Returns OK if package was not found or was successfully removed; FAIL
+    otherwise.
+    """
     with state.lock:
         pkg = state.index_store.get(pkg_name)
         if pkg is None:
@@ -108,87 +81,92 @@ def remove(state: State,
         else:
             return FAIL
 
-def query(state: State,
-          pkg_name: str) -> Result:
-    """For `QUERY` commands, the server returns `OK\n` if the package is
-    indexed. It returns `FAIL\n` if the package isn't indexed."""
+def query(state: State, pkg_name: str) -> Result:
+    """Returns OK if the package was found, FAIL otherwise.
+    """
     with state.lock:
-        if state.index_store.get(pkg_name):
-            return OK
-        else:
-            return FAIL
+        return OK if state.index_store.get(pkg_name) else FAIL
 
-def parse_command(data: bytes) -> Tuple[str, str, List[str]]:
-    if not data.endswith(b'\n'):
+def parse_command(raw_command: ByteString) -> Tuple[str, str, List[str]]:
+    """Given a byte string read from a socket, validates that the sent command
+    is properly formed and returns a tuple of the command's components
+    (command, package name, depdency list).
+    """
+    if not raw_command.endswith(b'\n'):
         raise CommandParsingError()
 
-    parts = data.decode().strip().split('|')
+    parts = raw_command.decode().strip().split('|')
     if len(parts) != 3:
         raise CommandParsingError()
 
-    if parts[0] not in COMMANDS:
+    command, pkg_name, deplist_string = parts
+
+    if command not in COMMANDS:
         raise CommandParsingError()
 
-    if re.match(PKGNAME_RE, parts[1]) is None:
+    if re.match(PKGNAME_RE, pkg_name) is None:
         raise CommandParsingError()
 
-    deplist_string = parts[2]
     deps = []
-    if len(deplist_string) > 0 and parts[0] == 'INDEX':
+    if len(deplist_string) > 0 and command == 'INDEX':
         if re.match(DEPLIST_RE, deplist_string) is None:
             raise CommandParsingError()
         deps = list(filter(lambda s: len(s) > 0, deplist_string.split(',')))
 
-    return (parts[0], parts[1], deps)
+    return (command, pkg_name, deps)
+
+def dispatch(state: State, raw_command: ByteString) -> Result:
+    """Given a reference to our shared thread state and a raw command, this function:
+        * validates the command
+        * parses the command
+        * runs the associated command function
+        * returns a suitable Result bytestring
+    """
+    try:
+        (command, pkg_name, deps) = parse_command(raw_command)
+
+        if command == 'QUERY':
+            return query(state, pkg_name)
+
+        if command == 'REMOVE':
+            return remove(state, pkg_name)
+
+        if command == 'INDEX':
+            return index(state, pkg_name, deps)
+
+        raise RuntimeError()
+
+    except CommandParsingError:
+        return ERROR
 
 class IndexerRequestHandler(BaseRequestHandler):
     def handle(self) -> None:
-        cur_thread = threading.current_thread()
-        print("Incoming client connection on ", cur_thread.name)
-        # TODO less tight loop. I tried both select and poll but couldn't get behavior I wanted.
+        """This request handler starts a loop for listening for incoming
+        commands. For each command, it attempts to parse it (sending ERROR if
+        that fails) and execute it. It stops looping when it can no longer read
+        from its associated socket.
+        """
         while True:
-            try:
-                command = self.request.recv(1024)
-                (command, pkg_name, deps) = parse_command(command)
-
-                actions = {'QUERY': partial(query, self.state, pkg_name),
-                           'REMOVE': partial(remove, self.state, pkg_name),
-                           'INDEX': partial(index, self.state, pkg_name, deps)}
-
-                self.request.send(actions[command]().bytes)
-            except CommandParsingError as e:
-                try:
-                    print('GOT BROKEN MESSAGE ', command.decode())
-                    self.request.send(ERROR.bytes)
-                except BrokenPipeError as e:
-                    print("Tried to report error to client, got broken pipe")
-                    break
-            except BrokenPipeError as e:
-                print("Client connection closed")
+            raw_command = self.request.recv(1024)
+            if not raw_command:
                 break
-            except ConnectionResetError as e:
-                print("Client connection closed")
-                break
-        print('Ceasing')
-
-class IndexerServer(ThreadingTCPServer):
-    request_queue_size = 128
-    allow_reuse_address = True
+            result = dispatch(self.state, raw_command)
+            self.request.send(result)
 
 def main():
+    """Entry point for this module. Starts up the TCP server and sets up the
+    shared thread state in an immutable data structure (a named tuple).
+    """
     HOST, PORT = ('127.0.0.1', 8080)
     print("Running at {}:{}".format(HOST, PORT))
 
-    index_store = {}
-    lock = Lock()
-    state = State(index_store, lock)
-    IndexerRequestHandler.state = state
+    IndexerRequestHandler.state = State({}, Lock())
 
-    server = IndexerServer((HOST, PORT), IndexerRequestHandler)
+    ThreadingTCPServer.allow_reuse_address = True
+    ThreadingTCPServer.request_queue_size = 128
+
+    server = ThreadingTCPServer((HOST, PORT), IndexerRequestHandler)
     server_thread = Thread(target=server.serve_forever)
     server_thread.daemon = True
     server_thread.start()
     server_thread.join()
-
-if __name__ == '__main__':
-    main()
