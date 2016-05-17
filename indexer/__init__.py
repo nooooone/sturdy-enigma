78c8c94b41e719b75aea1fef5f4372cd81226c4f
nooooone <hj9lt@slipry.net>
2016-05-17 13:43:55 -0700

    Big push to implement actual business logic
    * Adds thread lock mechanism
    * Adds State tuple to manage shared state
    * Adds an in memory datastore
    * Implements each command

--- indexer/__init__.py
+++ indexer/__init__.py
@@ -1,13 +1,16 @@
+from collections import namedtuple
+from functools import partial
 import re
 from socketserver import ThreadingTCPServer, BaseRequestHandler
-from threading import Thread
-from typing import List, Tuple, ByteString
-
-# TODO should use a queue for destructive operations
+import threading
+from threading import Thread, Lock
+from typing import List, Tuple, ByteString, Dict
 
 COMMANDS = ['INDEX', 'REMOVE', 'QUERY']
-PKGNAME_RE = re.compile(r'^[a-z]+$')
-DEPLIST_RE = re.compile(r'^([a-z],?)+$')
+PKGNAME_RE = re.compile(r'^[a-z0-9-_+A-Z]+$')
+DEPLIST_RE = re.compile(r'^([a-z0-9-_+A-Z],?)+$')
+IndexStore = Dict
+State = namedtuple('State', ['index_store', 'lock'])
 
 class CommandParsingError(Exception): pass
 
@@ -15,13 +18,14 @@ class Package:
     """Represents a package. The only data enumerated in the challenge
     description are package name and its dependencies, so that's all that is
     tracked here."""
-    __slots__ = ['dependencies', 'name']
+    __slots__ = ['dependencies', 'name', 'children']
 
     def __init__(self,
                  name: str,
-                 dependencies: List['Package']) -> None:
+                 dependencies: List[str]) -> None:
         self.name = name
         self.dependencies = dependencies
+        self.children = []
 
     def __hash__(self) -> int:
         return hash(self.name)
@@ -31,10 +35,6 @@ class Package:
 
     def __str__(self) -> str:
         return self.name
-
-# TODO i might not need this; this is just for global state tracking. Will
-# either contain some kind of package lookup or cache structure, if anything.
-class Data: pass
 
 class Result:
     """Contains a response code as self.code; one of 'fail', 'ok', or 'error'.
@@ -65,24 +65,57 @@ ERROR = Result('error')
 FAIL = Result('fail')
 OK = Result('ok')
 
-def depends(pkg0: Package,
-            pkg1: Package) -> bool:
-    """Given two packages, returns whether or not pkg0 depends no pkg1"""
-    return False
+def index(state: State,
+          pkg_name: str,
+          deps: List[str]) -> Result:
+    """For `INDEX` commands, the server returns `OK\n` if the package could be
+    indexed or if it was already present. It returns `FAIL\n` if the package
+    cannot be indexed because some of its dependencies aren't indexed yet and
+    need to be installed first."""
+    with state.lock:
+        pkg = state.index_store.get(pkg_name)
 
-def index(data: Data,
-          pkg0: Package,
-          deps: List[Package]) -> Result:
-    return ERROR
+        if pkg is not None:
+            return OK
 
-def remove(data: Data,
-           pkg0: Package) -> Result:
-    return ERROR
+        dep_pkgs = map(lambda d: state.index_store.get(d), deps)
+        if not all(dep_pkgs):
+            return FAIL
+        else:
+            index_store[pkg_name] = Package(pkg_name, deps, [])
+            for dep_pkg in dep_pkgs:
+                dep_pkg.children.append(pkg)
+            return OK
 
-def query(data: Data,
-          pkg0: Package) -> Result:
-    return ERROR
+def remove(state: State,
+           pkg_name: str) -> Result:
+    """For `REMOVE` commands, the server returns `OK\n` if the package could be
+    removed from the index. It returns `FAIL\n` if the package could not be
+    removed from the index because some other indexed package depends on it. It
+    returns `OK\n` if the package wasn't indexed."""
+    with state.lock:
+        pkg = state.index_store.get(pkg_name)
+        if pkg is None:
+            return OK
 
+        if len(pkg.children) == 0:
+            del state.index_store[pkg_name]
+            for dep_name in pkg.dependencies:
+                dep_pkg = state.index_store.get(dep_name)
+                dep_pkg.children.remove(pkg.name)
+            return OK
+        else:
+            return FAIL
+
+def query(state: State,
+          pkg_name: str) -> Result:
+    """For `QUERY` commands, the server returns `OK\n` if the package is
+    indexed. It returns `FAIL\n` if the package isn't indexed."""
+    with state.lock:
+        if state.index_store.get(pkg_name):
+            return OK
+        else:
+            return FAIL
 
 def parse_command(data: bytes) -> Tuple[str, str, List[str]]:
     if not data.endswith(b'\n'):
@@ -109,15 +142,19 @@ def parse_command(data: bytes) -> Tuple[str, str, List[str]]:
 
 class IndexerRequestHandler(BaseRequestHandler):
     def handle(self) -> None:
-        print("Incoming client connection, starting poll on socket")
+        cur_thread = threading.current_thread()
+        print("Incoming client connection on ", cur_thread.name)
         # TODO less tight loop. I tried both select and poll but couldn't get behavior I wanted.
         while True:
             try:
                 command = self.request.recv(1024)
                 (command, pkg_name, deps) = parse_command(command)
-                # TODO if mutable, acquire a lock (on what?)
-                # TODO don't need to always encode this
-                self.request.send(OK.bytes)
+
+                actions = {'QUERY': partial(query, pkg_name),
+                           'REMOVE': partial(remove, pkg_name),
+                           'INDEX': partial(index, pkg_name, deps)}
+
+                self.request.send(actions[command]().bytes)
             except CommandParsingError as e:
                 try:
                     print('GOT BROKEN MESSAGE ', command.decode())
@@ -140,6 +177,12 @@ class IndexerServer(ThreadingTCPServer):
 def main():
     HOST, PORT = ('127.0.0.1', 8080)
     print("Running at {}:{}".format(HOST, PORT))
+
+    index_store = {}
+    lock = Lock()
+    state = State(index_store, lock)
+    IndexerRequestHandler.state = state
+
     server = IndexerServer((HOST, PORT), IndexerRequestHandler)
     server_thread = Thread(target=server.serve_forever)
     server_thread.daemon = True
